0b04805389f748e32c036a677f29a588e8e3bd7f
Andrei Gavrilov <andrei.a.gavrilov@netcracker.com>
2023-06-16 15:24:35 +0300

    almost fixed

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.booking.enums.BookingState;
 import ru.practicum.shareit.item.repository.CommentRepository;
 import ru.practicum.shareit.request.mapper.ItemRequestMapper;
 import ru.practicum.shareit.booking.dto.BookingAllFieldsDto;
@@ -189,6 +190,7 @@ public class ItemServiceImpl implements ItemService {
         if (bookings != null)
             return bookings.stream()
                     .filter(booking -> booking.getStart().isAfter(now()))
+                    .filter(booking -> !booking.getStatus().equals(BookingState.REJECTED.name()))
                     .min(comparing(BookingAllFieldsDto::getEnd))
                     .orElse(null);
         else
@@ -198,7 +200,7 @@ public class ItemServiceImpl implements ItemService {
     private BookingAllFieldsDto getLastItem(List<BookingAllFieldsDto> bookings) {
         if (bookings != null)
             return bookings.stream()
-                    .filter(booking -> booking.getEnd().isBefore(now()))
+                    .filter(booking -> booking.getEnd().minusHours(1).isBefore(now()))
                     .max(comparing(BookingAllFieldsDto::getEnd))
                     .orElse(null);
         else
